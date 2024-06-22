1b051c74838a924d33269282596a3d5f8a290261
jws2170 <mpart2077@gmail.com>
2024-06-23 03:25:22 +0900

    Fix | ChangeFlipper

--- Assets/01.Scripts/ItemScript/ChangeFlipper.cs
+++ Assets/01.Scripts/ItemScript/ChangeFlipper.cs
@@ -9,8 +9,20 @@ public class ChangeFlipper : MonoBehaviour
     [SerializeField] private float maxScale = default;
     [SerializeField] private float itmeDurationTime = default;
 
+    private bool isChange = false;
+
     public GameObject leftPin = null;
     public GameObject rightPin = null;
+
+    public float xpos;
+    public float ypos;
+    public Vector2 newposition;
+    public SetBlind AppearBlind;
+
+    private void Start()
+    {
+        PosSet();
+    }
 
     private void Update()
     {
@@ -20,20 +32,23 @@ public class ChangeFlipper : MonoBehaviour
 
     private void SetFlipperEvent()
     {
-        float choseEvent = Random.Range(0, 1);
+        float choseEvent = Random.Range(0, 2);
 
-        switch (choseEvent)
+        if(isChange == false)
         {
-            case 0:
-                {
-                    SetSamllFlipper();
-                    break;
-                }
-            case 1:
-                {
-                    SetSamllFlipper();
-                    break;
-                }
+            switch (choseEvent)
+            {
+                case 0:
+                    {
+                        SetSamllFlipper();
+                        break;
+                    }
+                case 1:
+                    {
+                        SetSamllFlipper();
+                        break;
+                    }
+            }
         }
     }
 
@@ -49,12 +64,14 @@ public class ChangeFlipper : MonoBehaviour
 
     private void SetFlipperSize(float size)
     {
+        isChange = true;
         leftPin.transform.localScale = new Vector3(-size, 1f, 1f);
         rightPin.transform.localScale = new Vector3(size, 1f, 1f);
     }
 
     private void SetFlipperOriginSize(float size)
     {
+        isChange = false;
         leftPin.transform.localScale = new Vector3(-size, 1f, 1f);
         rightPin.transform.localScale = new Vector3(size, 1f, 1f);
     }
@@ -74,11 +91,25 @@ public class ChangeFlipper : MonoBehaviour
         SetFlipperSize(oringScale);
     }
 
+    void PosSet()
+    {
+        xpos = Random.Range(-160, 160);
+        ypos = Random.Range(-200, 270);
+        newposition = new Vector2(xpos, ypos);
+        transform.position = newposition;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Ball")
         {
             SetFlipperEvent();
+            PosSet();
+        }
+        else if (collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Obstacle")
+        {
+            PosSet();
+            Debug.Log("¥Í¿Ω");
         }
     }
 }
